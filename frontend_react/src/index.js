70debdc666b3b1e186dee6656d2c3d7fe0f6b1d0
jeffrey-huang-yz <137465385+jeffrey-huang-yz@users.noreply.github.com>
2024-02-18 21:32:58 -0500

    Refactor routing in App.js and index.js

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -7,9 +7,10 @@ import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
+    
   
 );
 
