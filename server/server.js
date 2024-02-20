4b8613853b9a0b59feb14120bc2bad6a38a40437
jeffrey-huang-yz <137465385+jeffrey-huang-yz@users.noreply.github.com>
2024-02-20 14:16:13 -0500

    Add console.log to print req.user

--- server/server.js
+++ server/server.js
@@ -327,6 +327,7 @@ accessType: 'offline', approvalPrompt: 'force' }), (req, res, next) => {
   app.get('/googleuser/data'), async (req, res, next) => {
     
     try {
+      console.log(req.user);
       if(req.user){
         console.log(req.user.userId);
         const sessionData = req.session;
