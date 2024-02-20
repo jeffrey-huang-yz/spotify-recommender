f337e1f999fb5a2cecb9c912b831450fb93c0e9c
jeffrey-huang-yz <137465385+jeffrey-huang-yz@users.noreply.github.com>
2024-02-20 01:32:19 -0500

    Remove unnecessary redirect in server.js

--- server/server.js
+++ server/server.js
@@ -306,7 +306,7 @@ accessType: 'offline', approvalPrompt: 'force' }));
   app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
     // Redirect to your frontend application with user data in query parameters
     res.redirect(`https://diskovery-ljvy.onrender.com/login/?userId=${req.user.userId}&email=${req.user.email}`);
-    res.redirect('https://diskovery.onrender.com/googleuser/data');
+    
   });
   
   app.get('/googleuser/data', async (req, res) => {
