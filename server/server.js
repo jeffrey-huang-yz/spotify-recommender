9aabc0e9aa35fc09a101d344e1cc9867e0e6c1da
jeffrey-huang-yz <137465385+jeffrey-huang-yz@users.noreply.github.com>
2024-02-20 02:41:14 -0500

    Update userId assignment in server.js

--- server/server.js
+++ server/server.js
@@ -325,7 +325,7 @@ accessType: 'offline', approvalPrompt: 'force' }));
       console.log(sessionData);
         // Check if user is authenticated
         
-        const userId = JSON.stringify(req.session.passport.user);
+        const userId = JSON.stringify(req.session.passport);
 
         // Find user in the database
         try {
