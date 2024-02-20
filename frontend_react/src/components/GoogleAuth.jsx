2e85dca81f5aafd20bb1fa2a46098597d4f8abc1
jeffrey-huang-yz <137465385+jeffrey-huang-yz@users.noreply.github.com>
2024-02-20 16:14:03 -0500

    Update Google authentication URL

--- frontend_react/src/components/GoogleAuth.jsx
+++ frontend_react/src/components/GoogleAuth.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 
 const GoogleAuth = () => {
     useEffect(() => {
-        window.location.href = 'https://diskovery.onrender.com/auth/google/callback';
+        window.location.href = 'https://diskovery.onrender.com/auth/google';
     }, []);
 
     return <div>Redirecting to Google authentication...</div>;
