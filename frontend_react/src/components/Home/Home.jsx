b031cf6cd64ec67e4b7f9f29241f0f09419a56c9
jeffrey-huang-yz <137465385+jeffrey-huang-yz@users.noreply.github.com>
2024-02-20 17:01:14 -0500

    Refactor user data fetching in Home component

--- frontend_react/src/components/Home/Home.jsx
+++ frontend_react/src/components/Home/Home.jsx
@@ -90,19 +90,11 @@ function Home({ selectedPlaylistId, selectedPlaylistName, onSearch }) {
 
   useEffect(() => {
     const fetchUser = async () => {
-      try {
-        const response = await axios.get('https://diskovery.onrender.com/googleuser/data', { withCredentials: true });
-        setUser(response.data);
-        setRefreshUserData(false); // Reset the refresh trigger
-      } catch (error) {
-        if (error.response && error.response.status === 401) {
-          // The user is not authenticated, redirect to the Google OAuth2 authorization page
-          window.location.href = '/auth/google';
-        } else {
-          console.error(error);
-        }
-      }
+      const response = await axios.get('https://diskovery.onrender.com/googleuser/data', { withCredentials: true });
+      setUser(response.data);
+      setRefreshUserData(false); // Reset the refresh trigger
     };
+
 
     const fetchRecentlyPlayedTracks = async () => {
       try {
